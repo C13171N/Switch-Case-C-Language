c0958d458032e043b4c6bd215fd77be9584d22f1
C13171N <118844424+C13171N@users.noreply.github.com>
2023-01-25 22:47:11 -0300

    Add files via upload

---
+++ SWITCH CASE PROJETO WILSON.cpp
@@ -0,0 +1,181 @@
+switch (resp) {
+		  case 1: {
+			printf(" Informe o volume em Litros: ");
+		    scanf("%f", & Volume);
+		    printf(" %.2fl equivale a %.2fml", Volume, LpML(Volume));
+		  	break;
+		  }
+		 case 2:{
+			printf("Informe o volume em litros: "); 
+			scanf("%f", & Volume);
+			printf(" %.2fl equivale a %.2fmc",Volume,LpMC(Volume));
+			break;
+		 }
+		 case 3:{
+			printf("Informe o volume em litros: ");
+			scanf("%f", & Volume); 
+			printf(" %.2fl equivale a %.2fpc",Volume,LpPC(Volume));
+			break;
+		 }
+		 case 4:{
+			printf("Informe o volume em litros: ");
+			scanf("%f", & Volume);
+			printf(" %.2fl equivale a %.2fpi",Volume,LpPI(Volume));
+			break;
+		 }
+		 case 5:{
+			printf("Informe o volume em litros: ");
+			scanf("%f", & Volume);
+			printf(" %.2fl equivale a %.2fola",Volume,LpOLA(Volume));
+			break;
+		 }
+		 case 6:{
+			printf("Informe o volume em mililitros: ");
+			scanf("%f", & Volume);
+			printf(" %.2fml equivale a %.2fl",Volume,MLpL(Volume));
+			break;
+		 }
+		 case 7:{
+			printf("Informe o volume em militros: ");
+			scanf("%f", & Volume);
+			printf(" %.2fml equivale a %.2fmc",Volume,MLpMC(Volume));
+			break;
+		 }
+		 case 8:{
+			printf("Informe o volume em mililitros: ");
+			scanf("%f", & Volume);
+			printf(" %.2fml equivale a %.2fpc",Volume,MLpPC(Volume));
+			break;
+		 }
+		 case 9:{
+			printf("Informe o volume em mililitros: ");
+			scanf("%f", & Volume);
+			printf(" %.2fml equivale a %.2fpi",Volume,MLpPI(Volume));
+			break;
+		 }
+		 case 10:{
+			printf("Informe o volume em mililitros: ");
+			scanf("%f", & Volume);
+			printf(" %.2fml equivale a %.2fola",Volume,MLpOLA(Volume));
+			break;
+		 }
+		 case 11:{
+			printf("Informe o volume em metros cubicos: ");
+			scanf("%f", & Volume);
+			printf(" %.2fmc equivale a %.2fl",Volume,MCpL(Volume));
+			break;
+		 }
+		 case 12:{
+			printf("Informe o volume em metros cubicos: ");
+			scanf("%f", & Volume);
+			printf(" %.2fmc equivale a %.2fml",Volume,MCpML(Volume));
+			break;
+		 }
+		 case 13:{
+			printf("Informe o volume em metros cubicos: ");
+			scanf("%f", & Volume);
+			printf(" %.2fmc equivale a %.2fpc",Volume,MCpPC(Volume));
+			break;
+		 }
+		 case 14:{
+			printf("Informe o volume em metros cubicos: ");
+			scanf("%f", & Volume);
+			printf(" %.2fmc equivale a %.2fpi",Volume,MCpPI(Volume));
+			break;
+		 }
+		 case 15:{
+			printf("Informe o volume em metros cubicos: ");
+			scanf("%f", & Volume);
+			printf(" %.2fmc equivale a %.2fola",Volume,MCpOLA(Volume));
+			break;
+		 }
+		 case 16:{
+			printf("Informe o volume em polegadas cubicas: ");
+			scanf("%f", & Volume);
+			printf(" %.2fpc equivale a %.2fl",Volume,PCpL(Volume));
+			break;
+		 }
+		 case 17:{
+			printf("Informe o volume em polegadas cubicas: ");
+			scanf("%f", & Volume);
+			printf(" %.2fpc equivale a %.2fml",Volume,PCpML(Volume));
+			break;
+		 }
+		 case 18:{
+			printf("Informe o volume em polegadas cubicas: ");
+			scanf("%f", & Volume);
+			printf(" %.2fpc equivale a %.2fmc",Volume,PCpMC(Volume));
+			break;
+		 }
+		 case 19:{
+			printf("Informe o volume em polegadas cubicas: ");
+			scanf("%f", & Volume);
+			printf(" %.2fpc equivale a %.2fpi",Volume,PCpPI(Volume));
+			break;
+		 }
+		 case 20:{
+			printf("Informe o volume em polegadas cubicas: ");
+			scanf("%f", & Volume);
+			printf(" %.2fpc equivale a %.2fola",Volume,PCpOLA(Volume));
+			break;
+		 }
+		 case 21:{
+			printf("Informe o volume em pintos imperiais: ");
+			scanf("%f", & Volume);
+			printf(" %.2fpi equivale a %.2fl",Volume,PIpL(Volume));
+			break;
+		 }
+		 case 22:{
+			printf("Informe o volume em pintos imperiais: ");
+			scanf("%f", & Volume);
+			printf(" %.2fpi equivale a %.2fml",Volume,PIpML(Volume));
+			break;
+		 }
+		 case 23:{
+			printf("Informe o volume em pintos imperiais: ");
+			scanf("%f", & Volume);
+			printf(" %.2fpi equivale a %.2fmc",Volume,PIpMC(Volume));
+			break;
+		 }
+		 case 24:{
+			printf("Informe o volume em pintos imperiais: ");
+			scanf("%f", & Volume);
+			printf(" %.2fpi equivale a %.2fpc",Volume,PIpPC(Volume));
+			break;
+		 }
+		 case 25:{
+			printf("Informe o volume em pintos imperiais: ");
+			scanf("%f", & Volume);
+			printf(" %.2fpi equivale a %.2fola",Volume,PIpOLA(Volume));
+			break;
+		 }
+		 case 26:{
+			printf("Informe o volume em oncas liquidas americanas: ");
+			scanf("%f", & Volume);
+			printf(" %.2fola equivale a %.2fl",Volume,OLApL(Volume));
+			break;
+		 }
+		 case 27:{
+			printf("Informe o volume em oncas liquidas americanas: ");
+			scanf("%f", & Volume);
+			printf(" %.2fola equivale a %.2fml",Volume,OLApML(Volume));
+			break;
+		 }
+		 case 28:{
+			printf("Informe o volume em oncas liquidas americanas: ");
+			scanf("%f", & Volume);
+			printf(" %.2fola equivale a %.2fmc",Volume,OLApMC(Volume));
+			break;
+		 } 
+		 case 29:{
+			printf("Informe o volume em oncas liquidas americanas: ");
+			scanf("%f", & Volume);
+			printf(" %.2fola equivale a %.2fpc",Volume,OLApPC(Volume));
+			break;
+		 }
+		 case 30:{
+		    printf("Informe o volume em oncas liquidas americanas")
+		    scanf("%f", & Volume);
+		    printf(" %.2fola equivale a %.2fpi",Volume,OLApPI(Volume));
+			break;
+		 }
